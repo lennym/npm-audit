bbc9504d678a73d7c69c9d987735c89f761960ae
Leonard Martin <leonard.martin@gmail.com>
2018-08-01 16:09:26 +0100

    Improve log output

--- index.js
+++ index.js
@@ -4,20 +4,30 @@ const levels = ['low', 'moderate', 'high', 'critical'];
 
 module.exports = options => {
 
-  const baseLevel = levels.indexOf(options.level || 'high');
+  const baseLevel = options.level || 'high';
+
+  if (levels.indexOf(baseLevel) === -1) {
+    console.error(`Invalid argument --level: ${baseLevel}`);
+    console.error(` --level should be one of ${levels.join(', ')}`);
+  }
 
   console.log(`Scanning for vulnerabilities...`);
   exec('npm audit --json', (err, stdout, stderr) => {
     const response = JSON.parse(stdout);
     const vulns = response.metadata.vulnerabilities;
-    const failed = levels.reduce((f, level, i) => {
+    const failed = levels.reduce((count, level, i) => {
       console.log(`${level}: ${' '.repeat(10 - level.length)}${vulns[level]}`);
-      return f || (i >= baseLevel && vulns[level] > 0);
-    }, false);
-
+      if (i >= levels.indexOf(baseLevel)) {
+        return count + vulns[level]);
+      }
+      return count;
+    }, 0);
+    console.log();
     if (failed) {
+      console.log(`${failed} vulnerabilitie(s) of level "${baseLevel}" or above detected.`);
       process.exit(1);
     }
+    console.log(`No vulnerabilities of level "${baseLevel}" or above detected.`);
   });
 
 };
