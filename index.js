4c9e3120b02f0b3c6d5644f2119b95d4b9f7f65b
avifried <avi@innovid.com>
2019-04-15 15:10:40 +0300

    18329 added support for differentiating dev deps

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const {spawnSync} = require('child_process');
 
 const levels = ['low', 'moderate', 'high', 'critical'];
 
+const reduceDevVulnerabilities = (level, count, auditResponse) => {
+  const numberOfDevVulns = Object.values(auditResponse.advisories)
+    .reduce((count, advisory) => {
+      if (advisory.severity === level) {
+        return count + advisory.findings[0].paths.length;
+      }
+    }, 0);
+  return count - numberOfDevVulns;
+};
+
 module.exports = options => {
   const baseLevel = options.level || 'high';
-
+  const prod = options.prod || false;
   if (levels.indexOf(baseLevel) === -1) {
     console.error(`Invalid argument --level: ${baseLevel}`);
     console.error(` --level should be one of ${levels.join(', ')}`);
@@ -24,6 +34,9 @@ module.exports = options => {
       if (i >= levels.indexOf(baseLevel)) {
         return count + vulns[level];
       }
+      if (prod) {
+        count = reduceDevVulnerabilities(level, count, response);
+      }
       return count;
     }, 0);
     console.log();
@@ -31,7 +44,8 @@ module.exports = options => {
       console.log(`${failed} vulnerabilitie(s) of level "${baseLevel}" or above detected.`);
       process.exit(1);
     }
-    console.log(`No vulnerabilities of level "${baseLevel}" or above detected.`);
+    console.log(`No vulnerabilities of level "${baseLevel}" or above detected ` +
+      `${prod ? '(filtered out dev dependencies)' : ''}.`);
   } catch (e) {
     console.error(e);
     process.exit(e.code || 1);
