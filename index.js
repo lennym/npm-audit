ce8500797763c38c4736553aae22adc8bdd40f1a
Shchori <nir.shc@innovid.com>
2019-07-15 14:34:37 +0300

    KAN-1809 ( Generic KAN issue ) [ remove dev dependencies and run in tmp folder ]

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const {spawnSync} = require('child_process');
-const fs = require("fs");
+const fs = require('fs');
 
 const levels = ['low', 'moderate', 'high', 'critical'];
 
-const tmpDir = "./tmpVul"
+const tmpDir = './tmpVul';
 
 const reduceDevVulnerabilities = (level, count, auditResponse) => {
   const numberOfDevVulns = Object.values(auditResponse.advisories)
@@ -24,18 +24,18 @@ module.exports = options => {
   }
   console.log(`Scanning for vulnerabilities...`);
   try {
-    !fs.existsSync(tmpDir) && fs.mkdirSync(tmpDir)
-    let packageJson =  JSON.parse(fs.readFileSync(`./package.json`));
+    !fs.existsSync(tmpDir) && fs.mkdirSync(tmpDir);
+    let packageJson = JSON.parse(fs.readFileSync(`./package.json`));
     packageJson.optionalDependencies = {};
     packageJson.devDependencies = {};
-    fs.writeFileSync(`${tmpDir}/package.json`,JSON.stringify(packageJson));
+    fs.writeFileSync(`${tmpDir}/package.json`, JSON.stringify(packageJson));
     fs.copyFileSync('./package-lock.json', `${tmpDir}/package-lock.json`);
-    const {status:npmInstallStatus,stderr:npmInstallErr} = spawnSync('npm', ['install',"--only=prod"],{cwd:tmpDir});
+    const {status: npmInstallStatus, stderr: npmInstallErr} = spawnSync('npm', ['install', '--only=prod'], {cwd: tmpDir});
     if (npmInstallStatus) {
-        console.error(npmInstallErr);
-        process.exit(1);
+      console.error(npmInstallErr);
+      process.exit(1);
     }
-    const {stdout, stderr} = spawnSync('npm', ['audit', '--json'],{cwd:tmpDir});
+    const {stdout, stderr} = spawnSync('npm', ['audit', '--json'], {cwd: tmpDir});
     if (stderr.toString() !== '') {
       console.error(stderr.toString());
       process.exit(1);
